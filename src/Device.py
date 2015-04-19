99505bae1bc0fbf645892223377251fd397c9109
Maciej Michalec <maciej.michalec@gmail.com>
2015-04-19 18:54:47 +0200

    Implemented drawing devices

--- src/Device.py
+++ src/Device.py
@@ -6,19 +6,61 @@ class Device:
         self.realCoordinates = coordinates
         self.name = name
         self.icon = icon
-        self.subsystem = ""
+        self.subsystemName = ""
         self.section = None
-        self.svgCoordinates = None
+        self.svgCoordinate = None
         self.numberInSection = None
         self.findAndSetSubsystemName()
 
     def findAndSetSubsystemName(self):
         nameParts = self.name.split("/")
         if len(nameParts) > 1:
-            self.subsystem = nameParts[1]
+            self.subsystemName = nameParts[1]
 
     def updateSvg(self):
-        pass
+        try:
+            self.checkNecessaryConditions()
+        except:
+            return
+        self.assignSvgCoordinate()
+        self.createSvgNode()
+
+
+    def checkNecessaryConditions(self):
+        exceptionMessage = "Device " + self.name + " cannot be drawn. "
+        if self.section == None:
+            print(exceptionMessage + "Probably it was not added to the appropriate section.")
+            raise Exception
+        if self.icon == None:
+            print(exceptionMessage + "No icon assigned to the device. ")
+            raise Exception
+        if self.subsystemName == "":
+            print(exceptionMessage + "Subsystem name is invalid.")
+            raise Exception
+
+    def assignSvgCoordinate(self):
+        if self.numberInSection == 0:
+            self.svgCoordinate = self.section.startCoordinate + self.section.getDistanceBetweenDevices()
+        else:
+            previousDeviceInSection = self.section.getDevice(self.numberInSection-1)
+            self.svgCoordinate = previousDeviceInSection.svgCoordinate + self.section.getDistanceBetweenDevices()
+
+    def createSvgNode(self):
+        svgFile = svg.SVG()
+        svgRoot = svgFile.getSvg()
+        subsystemNode = svgFile.getSubsystemZoomNode(self.subsystemName)
+
+        deviceElement = etree.SubElement(subsystemNode, "use")
+        deviceElement.attrib["id"] = self.generateSimpleName()
+        deviceElement.attrib["{http://www.w3.org/1999/xlink}href"] = "#" + self.icon.name
+        deviceElement.attrib["x"] = str(self.svgCoordinate)
+        deviceElement.attrib["y"] = "3585"
+
+    def generateSimpleName(self):
+        simpleName = self.name.lower()
+        simpleName = simpleName.replace("-", "")
+        simpleName = simpleName.replace("/", "")
+        return simpleName
 
     def isRingElement(self):
         return self.getSectionName().startswith("R1-")
--- tests/deviceTest.py
+++ tests/deviceTest.py
@@ -6,6 +6,8 @@ import unittest
 
 from src.Device import Device
 from src.Icon import Icon
+from src.Linac import Linac
+from src.LinacSection import LinacSection
 import src.svg as svg
 from lxml import etree
 
@@ -37,16 +39,16 @@ class TestDevice(unittest.TestCase):
 
     def testSubsystemName(self):
         device = Device("I-TL/VAC/I-TL-CAB02-VAC-IPCU1", None, None)
-        self.assertEqual(device.subsystem, "VAC")
+        self.assertEqual(device.subsystemName, "VAC")
 
         device = Device("I-S00/MAG/I-S00-MAG-COBX2", None, None)
-        self.assertEqual(device.subsystem, "MAG")
+        self.assertEqual(device.subsystemName, "MAG")
 
         device = Device("I-K01/RF/I-K01CAB03-RF-LLRFL1", None, None)
-        self.assertEqual(device.subsystem, "RF")
+        self.assertEqual(device.subsystemName, "RF")
 
         device = Device("I-S00/DIA/I-S00-DIA-SCRN1", None, None)
-        self.assertEqual(device.subsystem, "DIA")
+        self.assertEqual(device.subsystemName, "DIA")
 
     def testRingElement(self):
         device = Device("I-K01/VAC/I-K01CAB05-VAC-IPCU1", None, None)
@@ -62,6 +64,56 @@ class TestDevice(unittest.TestCase):
         device = Device("R1-SGA/MAG/R1-SGACAB14-MAG-PS04", None, None)
         self.assertEqual(device.isLinacElement(), False)
 
+    def testCheckingConditions(self):
+        deviceWithoutIcon = Device("A/B/C", None, None)
+        deviceWithoutIcon.section = True
+        with self.assertRaises(Exception):
+            deviceWithoutIcon.checkNecessaryConditions()
+
+        deviceWithoutSection = Device("D/E/F", Icon("test"), None)
+        with self.assertRaises(Exception):
+            deviceWithoutSection.checkNecessaryConditions()
+
+        deviceWithWrongSubsystem = Device("GHI", Icon("test"), None)
+        deviceWithWrongSubsystem.section = True
+        with self.assertRaises(Exception):
+            deviceWithWrongSubsystem.checkNecessaryConditions()
+
+    def testAssigningSvgCoordinate(self):
+        device = Device("abc/testDevice", None, None)
+        self.assertEqual(device.svgCoordinate, None)
+
+        linac = Linac()
+        linac.addSection("abc")
+        linac.addDevice(device)
+        device.assignSvgCoordinate()
+        testSection = linac.getSection("abc")
+        sectionStartCoordinate = testSection.startCoordinate
+        distanceBetweenDevices = testSection.getDistanceBetweenDevices()
+
+        self.assertEqual(device.svgCoordinate, sectionStartCoordinate+distanceBetweenDevices)
+
+    def testGenerateSimpleName(self):
+        device = Device("I-K01/VAC/I-K01CAB05-VAC-IPCU1", None, None)
+        self.assertEqual(device.generateSimpleName(), "ik01vacik01cab05vacipcu1")
+
+    def testDrawDevice(self):
+        icon = Icon("symbol-quadrupole.svg")
+        device = Device("I-K01/VAC/I-K01CAB05-VAC-IPCU1", icon, (113,122))
+        linac = Linac()
+        linac.addSection("I-K01")
+        linac.addDevice(device)
+
+        blankSVGpath = 'blank.svg'
+        svgTree = etree.parse(blankSVGpath)
+        svgRoot = svgTree.getroot()
+        svgFile = svg.SVG()
+        svgFile.setSvg(svgRoot)
+
+        device.updateSvg()
+        vacNode = svgFile.getSubsystemZoomNode("VAC")
+        deviceNode = svgFile.getElementById("ik01vacik01cab05vacipcu1", vacNode)
+        self.assertEqual(deviceNode.attrib["{http://www.w3.org/1999/xlink}href"], "#symbol-quadrupole")
 
     def tearDown(self):
         pass

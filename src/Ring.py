810a2d44a253739d6b2e36d10363c9e10b1b36a2
Maciej Michalec <maciej.michalec@gmail.com>
2015-04-25 22:06:57 +0200

    Use ArcDrawingTools in Ring class

--- src/Ring.py
+++ src/Ring.py
@@ -1,4 +1,4 @@
-from math import pi, cos, sin
+from ArcDrawingTools import ArcDrawingTools
 from RingSection import RingSection
 from svg import SVG
 from lxml import etree
@@ -36,50 +36,18 @@ class Ring:
             section.updateSvg()
         self.drawWhiteCircles()
 
-
     def drawWhiteCircles(self):
         self.svgFile = SVG()
-        self.svgRoot = self.svgFile.getSvg()
+        zoom1Node = self.svgFile.getZoom1Background()
+        zoom2Node = self.svgFile.getZoom2Background()
+        self.drawWhiteCircle(zoom1Node, -150, 50, "whiteCircleZoom1")
+        self.drawWhiteCircle(zoom2Node, -200, 80, "whiteCircleZoom2")
 
-        # white circle in zoom1
-        parentNode = self.svgFile.getZoom1Background()
-        path = self.describeArc(11989.075, 4827.0225, 2705.4143-150, 0, 359.99)
+    def drawWhiteCircle(self, parentNode, radiusOffset, strokeWidth, idName):
+        path = ArcDrawingTools.describeArcClockwise(11989.075, 4827.0225, 2705.4143+radiusOffset, 0, 359.99)
         sectionElement = etree.SubElement(parentNode, "path")
-        sectionElement.attrib["id"] = "whiteCircle"
+        sectionElement.attrib["id"] = idName
         sectionElement.attrib["d"] = path
         sectionElement.attrib["fill"] = "white"
         sectionElement.attrib["stroke"] = "white"
-        sectionElement.attrib["stroke-width"] = "50"
-
-        # white circle in zoom2
-        zoomNode = self.svgFile.getZoom2Background()
-        path = self.describeArc(11989.075, 4827.0225, 2705.4143-200, 0, 359.99)
-        sectionElement = etree.SubElement(zoomNode, "path")
-        sectionElement.attrib["id"] = "whiteBorder"
-        sectionElement.attrib["d"] = path
-        sectionElement.attrib["fill"] = "white"
-        sectionElement.attrib["stroke"] = "white"
-        sectionElement.attrib["stroke-width"] = "80"
-
-    def polarToCartesian(self, centerX, centerY, radius, angleInDegrees):
-        angleInRadians = (angleInDegrees-90.0) * pi / 180.0
-        #print "Angle in radians=" + str(angleInRadians)
-        x = centerX + (radius * cos(angleInRadians))
-        y = centerY + (radius * sin(angleInRadians))
-        return [x, y]
-
-
-    def describeArc(self, x, y, radius, startAngle, endAngle):
-        start = self.polarToCartesian(x, y, radius, endAngle)
-        #print("startX=" + str(start[0]) + ", startY=" + str(start[1]))
-        end = self.polarToCartesian(x, y, radius, startAngle)
-        #print("endX=" + str(end[0]) + ", endY=" + str(end[1]))
-
-        arcSweep = "0" if endAngle - startAngle <= 180 else "1"
-
-        d = "M " + str(start[0]) + " " + str(start[1]) + \
-             " A " + str(radius) + " " + str(radius) + " 0 " + arcSweep + " 0 "  +str(end[0]) + " " + str(end[1]) + \
-            " L " + str(x) + " " + str(y) + \
-            " L " + str(start[0]) + " " + str(start[1]) + " Z"
-
-        return d
+        sectionElement.attrib["stroke-width"] = str(strokeWidth)

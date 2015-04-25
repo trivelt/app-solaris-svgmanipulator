98643c592e22cecd3b0b7df0958d246a1a8db958
Maciej Michalec <maciej.michalec@gmail.com>
2015-04-25 23:02:36 +0200

    Implemented drawing Ring sections

--- src/RingSection.py
+++ src/RingSection.py
@@ -1,5 +1,6 @@
 from math import pi, cos, sin
 from Section import Section
+from ArcDrawingTools import ArcDrawingTools
 from lxml import etree
 import svg
 
@@ -22,82 +23,86 @@ class RingSection(Section):
         RingSection.id += 1
 
     def updateSvg(self):
-        self.drawSectionOnZoom1()
-        self.drawSectionOnZoom2()
-        self.drawBigText()
+        if self.isInUpperHalf():
+            clockwiseFunction = ArcDrawingTools.describePathForTextAnticlockwise
+        else:
+            clockwiseFunction = ArcDrawingTools.describePathForTextClockwise
+        self.drawSectionOnZoom1(clockwiseFunction)
+        self.drawSectionOnZoom2(clockwiseFunction)
 
-    def drawSectionOnZoom1(self):
+    def isInUpperHalf(self):
+        numbersOfAngle = [x for x in range(self.startAngle, self.endAngle)]
+        numbersOfUpperHalf = [x for x in range(-90, 90)]
+        listsIntersection = set(numbersOfAngle).intersection(numbersOfUpperHalf)
+        percentInUpperHalf = len(listsIntersection) / float(len(numbersOfAngle))
+        if percentInUpperHalf > 0.5:
+            return True
+        else:
+            return False
+
+    def drawSectionOnZoom1(self, clockwiseFunction):
+        self.drawBigColorfulCircle()
+        self.drawBigText(clockwiseFunction)
+
+    def drawBigColorfulCircle(self):
         zoomNode = self.svgFile.getZoom1Background()
-        self.drawColorfulCircle(zoomNode)
-
-    def drawColorfulCircle(self, parentNode):
-        path = self.describeArc(self.centerX, self.centerY, self.radius+100, self.startAngle, self.endAngle)
-        sectionElement = etree.SubElement(parentNode, "path")
-        sectionElement.attrib["id"] = self.shortName + "colorfulCircle"
-        sectionElement.attrib["d"] = path
-        sectionElement.attrib["fill"] = self.colour
-        sectionElement.attrib["stroke"] = "white"
-        sectionElement.attrib["stroke-width"] = "0"
-        #sectionElement.attrib["style"] = "opacity:0.5;"
+        sectionElement = self.drawColourfulSection(zoomNode, 100, 0, "colourfulCircle")
 
         descElement = etree.SubElement(sectionElement, "desc")
         descElement.attrib["id"] = self.shortName + "desc"
         descElement.text = "section=" + self.longName
 
-    def drawSectionOnZoom2(self):
+    def drawSectionOnZoom2(self, clockwiseFunction):
         zoomNode = self.svgFile.getZoom2Background()
-        path = self.describeArc(self.centerX, self.centerY, self.radius-150, self.startAngle, self.endAngle)
-        sectionElement = etree.SubElement(zoomNode, "path")
-        sectionElement.attrib["id"] = self.shortName + "colorfulBorder"
+        self.drawColourfulSection(zoomNode, -150, 20, "colorfulBorder")
+        self.drawSmallText(clockwiseFunction)
+
+    def drawColourfulSection(self, parentNode, radiusOffset, strokeWidth, idName):
+        path = ArcDrawingTools.describeArcClockwise(self.centerX, self.centerY, self.radius+radiusOffset, self.startAngle, self.endAngle)
+
+        sectionElement = etree.SubElement(parentNode, "path")
+        sectionElement.attrib["id"] = self.shortName + idName
         sectionElement.attrib["d"] = path
-        sectionElement.attrib["fill"] = "white"
+        sectionElement.attrib["fill"] = self.colour
         sectionElement.attrib["stroke"] =  self.colour
-        sectionElement.attrib["stroke-width"] = "20"
+        sectionElement.attrib["stroke-width"] = str(strokeWidth)
+        return sectionElement
 
-    def drawBigText(self):
-        start = self.polarToCartesian(self.centerX, self.centerY, self.radius, self.startAngle)
-        end = self.polarToCartesian(self.centerX, self.centerY, self.radius, self.endAngle)
+    def drawBigText(self, clockwiseFunction):
+        style = "font-size:200px;font-style:normal;"
+        zoomElement = self.svgFile.getZoom1Background()
+        if clockwiseFunction == ArcDrawingTools.describePathForTextAnticlockwise:
+            radiusOffset = -100
+        else:
+            radiusOffset = 35
+        self.drawText(zoomElement, radiusOffset, clockwiseFunction, style, "bigCaption")
 
+    def drawSmallText(self, clockwiseFunction):
+        style = "font-size:20px;font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;text-align:" \
+                "start;line-height:125%;letter-spacing:0px;word-spacing:0px;writing-mode:lr-tb;text-anchor:start;fill:" \
+                "#000000;fill-opacity:1;stroke:none;display:inline;font-family:Sans;-inkscape-font-specification:'Sans, Normal'"
+        zoomElement = self.svgFile.getZoom2Background()
+        if clockwiseFunction == ArcDrawingTools.describePathForTextAnticlockwise:
+            radiusOffset = -157
+        else:
+            radiusOffset = -143
+
+        self.drawText(zoomElement, radiusOffset, clockwiseFunction, style, "smallCaption")
+
+    def drawText(self, parentNode, radiusOffset, clockwiseFunction, style, idName):
         defs = self.svgFile.getElementById("defs8812", self.svgRoot)
         pathDef = etree.SubElement(defs, "path")
-        pathDef.attrib["id"] = self.shortName+"pathdef"
-        print self.longName + ": Start.x=" + str(start[0]) + ",start.y=" + str(start[1]) + ", end.x=" + str(end[0]) + ", end.y=" + str(end[1])
-        pathDef.attrib["d"] = "M " + str(start[0]) + " " + str(start[1]) + " S " + str(end[0]-200) + \
-            " " + str(end[1] - 500) + " " + str(end[0]) + " " + str(end[1])
+        pathDef.attrib["id"] = self.shortName + idName + "Path"
+        pathDef.attrib["d"] = clockwiseFunction(self.centerX, self.centerY, self.radius+radiusOffset, self.startAngle, self.endAngle)
 
-        zoomElement = self.svgFile.getZoom1Background()
-        textEleement = etree.SubElement(zoomElement, "text")
-        textEleement.attrib["x"] = "0"
-        textEleement.attrib["y"] = "0"
-        textEleement.attrib["font-size"] = "200"
-        textEleement.attrib["style"] = "stroke:#000000;"
-        textEleement.attrib["text-anchor"] = "middle"
+        textElement = etree.SubElement(parentNode, "text")
+        textElement.attrib["id"] = self.shortName + idName
+        textElement.attrib["x"] = "0"
+        textElement.attrib["y"] = "0"
+        textElement.attrib["style"] = style
+        textElement.attrib["text-anchor"] = "middle"
 
-        textPathElement = etree.SubElement(textEleement, "textPath")
-        textPathElement.attrib["{http://www.w3.org/1999/xlink}href"] = "#" + self.shortName+"pathdef"
+        textPathElement = etree.SubElement(textElement, "textPath")
+        textPathElement.attrib["{http://www.w3.org/1999/xlink}href"] = "#" + self.shortName + idName + "Path"
         textPathElement.attrib["startOffset"] = "50%"
         textPathElement.text = self.longName
-
-
-    def polarToCartesian(self, centerX, centerY, radius, angleInDegrees):
-        angleInRadians = (angleInDegrees-90.0) * pi / 180.0
-        #print "Angle in radians=" + str(angleInRadians)
-        x = centerX + (radius * cos(angleInRadians))
-        y = centerY + (radius * sin(angleInRadians))
-        return [x, y]
-
-
-    def describeArc(self, x, y, radius, startAngle, endAngle):
-        start = self.polarToCartesian(x, y, radius, endAngle)
-        #print("startX=" + str(start[0]) + ", startY=" + str(start[1]))
-        end = self.polarToCartesian(x, y, radius, startAngle)
-        #print("endX=" + str(end[0]) + ", endY=" + str(end[1]))
-
-        arcSweep = "0" if endAngle - startAngle <= 180 else "1"
-
-        d = "M " + str(start[0]) + " " + str(start[1]) + \
-             " A " + str(radius) + " " + str(radius) + " 0 " + arcSweep + " 0 "  +str(end[0]) + " " + str(end[1]) + \
-            " L " + str(x) + " " + str(y) + \
-            " L " + str(start[0]) + " " + str(start[1]) + " Z"
-
-        return d
--- tests/ringSectionTests.py
+++ tests/ringSectionTests.py
@@ -6,6 +6,7 @@ import unittest
 
 from src.Ring import Ring
 from src.RingSection import RingSection
+from src.ArcDrawingTools import ArcDrawingTools
 from src.Device import Device
 import src.svg as svg
 from lxml import etree
@@ -18,17 +19,39 @@ class TestRingSection(unittest.TestCase):
         svgRoot = svgTree.getroot()
         self.svgFile = svg.SVG()
         self.svgFile.setSvg(svgRoot)
+        RingSection.id = 1
 
     def testNewSection(self):
-        pass
+        section = RingSection("R1-01", "green", 15, 35)
+        self.assertEqual(section.longName, "R1-01")
+        self.assertEqual(section.shortName, "ringSection1")
+        self.assertEqual(section.startAngle, 15)
+        self.assertEqual(section.endAngle, 50)
 
-    def testUpdateSvg(self):
+    def testIsInUpperHalf(self):
+        upperSection = RingSection("InUpper", None, -30, 180)
+        lowerSection = RingSection("InLower", None, 100, 120)
+        self.assertEqual(upperSection.isInUpperHalf(), True)
+        self.assertEqual(lowerSection.isInUpperHalf(), False)
 
+    def testDrawZoom1Section(self):
         section = RingSection("name", "blue", 0, 90)
-        section.updateSvg()
+        section.drawSectionOnZoom1(ArcDrawingTools.describePathForTextAnticlockwise)
+        node = self.svgFile.getZoom1Background()
+        sectionNode = self.svgFile.getElementById("ringSection1colourfulCircle", node)
+        self.assertEqual(sectionNode.attrib["d"], "M 14794.4893 4827.0225 A 2805.4143 2805.4143 0 0 0 11989.075 "
+                                                  "2021.6082L 11989.075 4827.0225 L 14794.4893 4827.0225 Z")
+        self.assertEqual(sectionNode.attrib["fill"], "blue")
+
+    def testDrawBigCaption(self):
+        section = RingSection("R2-01", "yellow", 100,45)
+        section.drawSectionOnZoom1(ArcDrawingTools.describePathForTextAnticlockwise)
+        zoomNode = self.svgFile.getZoom1Background()
+        textNode = self.svgFile.getElementById("ringSection1bigCaption", zoomNode)
+        self.assertEqual(textNode.attrib["style"], "font-size:200px;font-style:normal;")
 
     def tearDown(self):
-        pass
+        RingSection.id = 1
 
 if __name__ == '__main__':
     unittest.main()

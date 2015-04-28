a79766db600dc458f008df073ea0098105278201
Maciej Michalec <maciej.michalec@uj.edu.pl>
2015-04-28 11:11:38 +0200

    Added new UI elements: SectionWidget and ColorChooser

---
+++ ui/ColorChooser.py
@@ -0,0 +1,22 @@
+from PyQt4.QtGui import QLabel, QColorDialog
+
+class ColorChooser(QLabel):
+    def __init__(self, parent):
+        QLabel.__init__(self, parent)
+        self.colorDialog = QColorDialog()
+        self.setToolTip("Choose section color")
+        self.selectedColor = "white"
+        self.updateLabelColor()
+
+    def mousePressEvent(self, QMouseEvent):
+        self.colorDialog.exec_()
+        self.selectedColor = self.colorDialog.selectedColor().name()
+        self.updateLabelColor()
+
+    def updateLabelColor(self):
+        styleSheet = "background-color:" + self.selectedColor + ";"
+        styleSheet += "border-radius:7px"
+        self.setStyleSheet(styleSheet)
+
+    def getSelectedColor(self):
+        return self.selectedColor

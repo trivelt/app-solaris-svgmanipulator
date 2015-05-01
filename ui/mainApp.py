ff00aba4ac7cab0cb961abc1077d46be324befc6
Maciej Michalec <maciej.michalec@gmail.com>
2015-05-01 18:38:07 +0200

    Implemented sections drawing from GUI

--- ui/mainApp.py
+++ ui/mainApp.py
@@ -42,6 +42,37 @@ class mainApp(QMainWindow):
 
     def loadDevices(self):
         svgDrawer = SvgDrawer()
+        svgDrawer.loadSvg("../src/blank.svg")
+        self.processLinacSections(svgDrawer)
+        self.processRingSections(svgDrawer)
         svgDrawer.drawAll()
         svgDrawer.saveSvg("../testImage.svg")
         print "Loading"
+
+    def processLinacSections(self, svgDrawer):
+        sectionsData = self.secondStep.getLinacData()
+        for section in sectionsData:
+            [name, color, sizeInPercent, displayedNameFlag, displayedName, subsectionsData] = self.processSectionsData(section)
+            widthInPixels = sizeInPercent*7770.0
+            svgDrawer.addSectionToLinac(name, color, widthInPixels)
+
+
+    def processRingSections(self, svgDrawer):
+        sectionsData = self.secondStep.getRingData()
+        for section in sectionsData:
+            [name, color, sizeInPercent, displayedNameFlag, displayedName, subsectionsData] = self.processSectionsData(section)
+
+            angleInDegrees = sizeInPercent*360.0
+            svgDrawer.addSectionToRing(name, color, angleInDegrees)
+
+    def processSectionsData(self, sectionsData):
+            subsectionsData = None
+            if len(sectionsData) > 5:
+                subsectionsData = sectionsData[5]
+            name = str(sectionsData[0])
+            sizeInPercent = float(str(sectionsData[1]).replace(",","."))/100.0
+            color = str(sectionsData[2])
+            displayedNameFlag = bool(sectionsData[3])
+            displayedName = str(sectionsData[4])
+
+            return [name, color, sizeInPercent, displayedNameFlag, displayedName, subsectionsData]

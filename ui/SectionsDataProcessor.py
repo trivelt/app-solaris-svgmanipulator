f77aeb9bfbb989e89857f23fb6a51e1eeb2a9868
Maciej Michalec <maciej.michalec@gmail.com>
2015-05-02 13:02:24 +0200

    Removed duplicate code

--- ui/SectionsDataProcessor.py
+++ ui/SectionsDataProcessor.py
@@ -4,29 +4,29 @@ class SectionsDataProcesssor:
     def __init__(self, svgDrawer):
         self.svgDrawer = svgDrawer
 
-
     def processLinacSections(self, sectionsData):
         for section in sectionsData:
-            [name, color, sizeInPercent, displayedNameFlag, displayedName, subsectionsData] = self.processSectionsData(section)
+            [name, color, sizeInPercent, displayedNameFlag, displayedName, subsectionsData] = self.processSectionData(section)
             widthInPixels = sizeInPercent*7770.0
-            self.svgDrawer.addSectionToLinac(name, color, widthInPixels)
-
+            newSection = self.svgDrawer.addSectionToLinac(name, color, widthInPixels)
 
     def processRingSections(self, sectionsData):
         for section in sectionsData:
-            [name, color, sizeInPercent, displayedNameFlag, displayedName, subsectionsData] = self.processSectionsData(section)
-
+            [name, color, sizeInPercent, displayedNameFlag, displayedName, subsectionsData] = self.processSectionData(section)
             angleInDegrees = sizeInPercent*360.0
-            self.svgDrawer.addSectionToRing(name, color, angleInDegrees)
+            newSection = self.svgDrawer.addSectionToRing(name, color, angleInDegrees)
 
-    def processSectionsData(self, sectionsData):
+    def processSectionData(self, sectionData):
+            [name, color, sizeInPercent, displayedNameFlag, displayedName] = self.processBaseSectionData(sectionData)
             subsectionsData = None
-            if len(sectionsData) > 5:
-                subsectionsData = sectionsData[5]
-            name = str(sectionsData[0])
-            sizeInPercent = float(str(sectionsData[1]).replace(",","."))/100.0
-            color = str(sectionsData[2])
-            displayedNameFlag = bool(sectionsData[3])
-            displayedName = str(sectionsData[4])
-
+            if len(sectionData) > 5:
+                subsectionsData = sectionData[5]
             return [name, color, sizeInPercent, displayedNameFlag, displayedName, subsectionsData]
+
+    def processBaseSectionData(self, sectionData):
+            name = str(sectionData[0])
+            sizeInPercent = float(str(sectionData[1]).replace(",","."))/100.0
+            color = str(sectionData[2])
+            displayedNameFlag = bool(sectionData[3])
+            displayedName = str(sectionData[4])
+            return [name, color, sizeInPercent, displayedNameFlag, displayedName]
